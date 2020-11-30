24b94f4a9e21e2814f30fe2f19ba9abb0ef0fcf6
YiLAMA <mynau7753191@gmail.com>
2020-11-30 23:14:36 +0300

    add cmd args

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import numpy as np
 import pandas as pd
 import csv
 import time
+
+
 # from memory_profiler import profile  # @profile  # Для теста объёма памяти
 
+# file_read = "incidents"
+# file_save = "counts"
+# M = "100"
 
 # @profile  # Для теста объема потребляемой программой памяти в процессе работы
 def get_counts(file_read, M, dT):
@@ -15,7 +20,7 @@ def get_counts(file_read, M, dT):
     3) категориальные признаки feature1 и feature1 не превосходит наш указанный M.
     """
     dict_counts = {}  # Cловарь, куда записываем id и counts (количество инцидентов удовл-щие условия задачи)
-    df = pd.read_csv(file_read)  # Читаем файл
+    df = pd.read_csv(f'incidents/' + file_read + ".csv")  # Читаем файл
     # Удаляем инциденты, которые не соответствуют нашим условиям М из арументов функции
     df = df.drop(df[(df.feature1 >= M) | (df.feature2 >= M)].index)
     array = df.sort_values('time').values  # Отсортируем по времени все инциденты
@@ -32,10 +37,11 @@ def get_counts(file_read, M, dT):
         feature2 = int(array[i][2])
         time_ = array[i][3] - dT  # Диапазон времени с учётом dT
         counts = ((box[feature1, feature2] - time_) > 0).sum()  # Считаем инциденты, попавшие в диапазон времени
-        box[feature1, feature2] = np.append(box[feature1, feature2], (array[i][3]))  # Передаём время текущего инцидента в box
+        box[feature1, feature2] = np.append(box[feature1, feature2],
+                                            (array[i][3]))  # Передаём время текущего инцидента в box
         dict_counts[int(array[i][0])] = counts  # Добавляем id и counts в наш словарь
 
-    with open(file_save, 'w') as f:  # Записываем в файл
+    with open(f'counts/' + file_save + ".csv", 'w') as f:  # Записываем в файл
         writer = csv.writer(f)
         writer.writerow(['id', 'counts'])  # Первой строчкой записываем заголовки для наших данных
         for key, value in sorted(dict_counts.items()):  # Записываем словарь в файл и сортируем по id
@@ -50,25 +56,35 @@ def main():
 
 
 if __name__ == '__main__':
-    file_read = input(
-        "Напишите название считываемого файла, без указания формата (.csv - это не надо),"
-        "откуда берём данные для работы: (в условии задачи, был такой пример: incidents)\n")
-    file_read = f'incidents/' + file_read + ".csv"
+    # Метод №1, с помощью argparse. Вызов из командной строки:
+    # Пример вызова из командной строки:
+    # python3 main.py -fr incidents -M 100 -dT 0.3 -fs countsTest
+    import argparse
 
-    file_save = input(
-        "Напишите название сохраняемого файла, куда выписываем результаты работы, тоже без указания формата (.csv):\n")
-    file_save = f'counts/' + file_save + ".csv"
+    parser = argparse.ArgumentParser(description='Тестовое задание. Описание именных аргументов')
+    parser.add_argument('-fr', dest="file_read", default='incidents', type=str,
+                        help="Название файла для чтения данных (без указания формата), например: incidents")
+    parser.add_argument('-M', dest="M", default=100, type=int,
+                        help="Аргумент M. Ограничение для катериальных признаков инцидента при чтении")
+    parser.add_argument('-dT', dest="dT", default=0.3, type=float,
+                        help="Аргумент dT. Диапазон времени при подсчете результатов совпадающих инцидентов")
+    parser.add_argument('-fs', dest="file_save", default='countsTest', type=str,
+                        help="Название файла, куда сохраняем данные (без указания формата), например: counts")
 
-    print("Укажите параметр dT для нашей функции, нецелое значение от 0 до 1: (Например 0.3)")
-    dT = float(input())
-    print("Укажите параметр M для нашей функции, целое значение от 0 до любое: (Например 100)")
-    M = int(input())
+    # globals().update(...) - чтобы поместить эти переменные в глобальное пространство имен.
+    globals().update(vars(parser.parse_args()))
+    args = parser.parse_args()
+    print(args)
 
-    # Данные для теста объема потребляемой программой памяти в процессе работы или для теста скорости работы
-    # file_read = 'incidents/incidents100KK.csv'
-    # file_save = 'counts/counts100KK.csv'
-    # dT = 0.3
-    # M = 100
-    # Примерное время работы функции 38 секунд.
+    # Метод №2, с помощью sys.argv. РАБОЧИЙ. Вызов из командной строки:
+    # Пример вызова из командной строки:
+    # python3 main.py get_counts incidents 100 0.3 counts
+    # import sys
+    # func_name = sys.argv[1]
+    # file_read = sys.argv[2]
+    # M = int(sys.argv[3])
+    # dT = float(sys.argv[4])
+    # file_save = sys.argv[5]
+    # getattr(sys.modules[__name__], func_name)(file_read, M, dT), (file_save)
 
     main()
